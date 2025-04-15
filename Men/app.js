5aad35ed6827da4eb88880145e9fdb66df5e373c
codingpratham <coding.job17@gmail.com>
2025-04-16 00:37:03 +0530

    middleware

--- Men/app.js
+++ Men/app.js
@@ -1,14 +1,30 @@
-const http = require("http")
+const express= require("express")
+const app = express()
 
-const server =  http.createServer((req,res)=>{
-    if(req.url === "/about"){
-        res.end("about page")
-    }else{
-        res.end("Kuch bhi h bhai")
-    }
+app.set('view engine','ejs')
+
+app.use((req,res,next)=>{
+    console.log('Middleware 1')
+    next()
 })
 
-server.listen(3000,()=>{
-    console.log("localhost is ready on theport 3000");
-    
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get ('/about',(req,res)=>{
+    res.send('About us')
+})
+
+app.get ('/contact',(req,res)=>{
+    res.send('Contact us')
+})
+
+app.get ('/services',(req,res)=>{
+    res.send('Services')
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+    console.log('http://localhost:3000')
 })
