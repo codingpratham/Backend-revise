9cbe3346718eaace8eaf2c33084db63eb12435e8
codingpratham <coding.job17@gmail.com>
2025-05-04 13:32:55 +0530

    .

--- drive/routes/user.routes.js
+++ drive/routes/user.routes.js
@@ -46,4 +46,30 @@ router.post('/register', async (req, res) => {
   res.status(201).json({ message: 'User created successfully' });
 });
 
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.post('/login', async (req, res) => {
+  const {email,password}  = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Please fill all fields' });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' });
+  }
+  res.status(200).json({ message: 'User found' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: 'Invalid credentials' });
+  }
+  res.redirect('/')
+  res.status(200).json({ message: 'Login successful' });
+})
+
 module.exports = router;
